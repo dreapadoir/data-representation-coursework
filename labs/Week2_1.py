7b04e98574d8f12c2d98bbfb98b64cddea92b56f
David Higgins <g00411302@gmit.ie>
2023-10-02 20:40:10 +0100

    add first for loop and get code to print D trains

--- labs/Week2_1.py
+++ labs/Week2_1.py
@@ -7,7 +7,22 @@ url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 page = requests.get(url)
 doc = parseString(page.content)
 
-print(doc.toprettyxml())
+#print(doc.toprettyxml())
 
 with open("trainxml.xml", "w") as xmlfp:
     doc.writexml(xmlfp)
+
+dTrains = []
+objTrainPositionNodes = doc.getElementsByTagName("objTrainPositions")
+for objTrainPositionNode in objTrainPositionNodes:
+    trainCodeNode = objTrainPositionNode.getElementsByTagName("TrainCode").item(0)
+    trainCode = trainCodeNode.firstChild.nodeValue
+    #print(trainCode)
+    #for train in trainCode:
+       # if train[0] == "D":
+       #     print(trainCode)
+        
+#print(dTrains)
+
+
+
