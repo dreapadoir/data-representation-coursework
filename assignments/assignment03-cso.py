ca5f57127bf440ca883e21cec5b29e492c1218e1
David Higgins <g00411302@gmit.ie>
2023-10-19 19:36:45 +0100

    add comments to assignment 3

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -1,10 +1,13 @@
+#script to pull data from CSO and write to .json file
+#author: David Higgins - G00411302
+
 import requests
 import json
 
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
 
 def getData():
-    response = requests.get(url, verify=False)
+    response = requests.get(url, verify=False)  #work computer is causing issues with SSL so I have to set verify to false to get this to run
     return response.json()
 
 if __name__ == "__main__":
