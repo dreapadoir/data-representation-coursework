1255d621d4bc12073f491b385a8d57d2eede78c2
David Higgins <g00411302@gmit.ie>
2023-10-19 19:34:04 +0100

    adding code for assignment 3

---
+++ assignments/assignment03-cso.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+
+def getData():
+    response = requests.get(url, verify=False)
+    return response.json()
+
+if __name__ == "__main__":
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(getData()), file=fp)
